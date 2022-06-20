3ab8599c0c8cd6dd986efdbb8783f651a5ee6d44
Guillermo Delgado <91484771+Froostx@users.noreply.github.com>
2022-06-20 11:16:20 -0300

    Adding classes

---
+++ src/uy/gruposoft/logica/Logica.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.gruposoft.logica;
+
+/**
+ *
+ * @author guill
+ */
+public class Logica {
+    
+}
