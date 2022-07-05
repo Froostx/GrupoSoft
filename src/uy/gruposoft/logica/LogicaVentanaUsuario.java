65c356e5b62759bc0513da0a7eca9f5b3413943e
Jose <aventmaster584@outlook.es>
2022-07-05 19:13:00 -0300

    ventana de usuario

--- src/uy/gruposoft/logica/LogicaVentanaUsuario.java
+++ src/uy/gruposoft/logica/LogicaVentanaUsuario.java
@@ -5,18 +5,38 @@
  */
 package uy.gruposoft.logica;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
-import uy.gruposoft.persistencia.Conexion;
+import uy.gruposoft.excepciones.UsuarioException;
+import uy.gruposoft.persistencia.PersistenciaUsuario;
+
+import uy.gruposoft.presentacion.VentanaDeUsuario;
 
 /**
  *
  * @author guill
  */
 public class LogicaVentanaUsuario {
+    
+    public static void mostrarUsuarios() 
+    {
+        PersistenciaUsuario logica = new PersistenciaUsuario();
+        
+         String[] nombresColumnas = {"username", "nombre", "apellido", "email", "contraseña", "fecha_alta", "fecha_baja"};
+       
+        
+        VentanaDeUsuario.tabla.setModel(FachadaLogica.cargarUsuario(nombresColumnas));
+        
+        VentanaDeUsuario.tabla.getColumnModel().getColumn(0).setPreferredWidth(60);
+        VentanaDeUsuario.tabla.getColumnModel().getColumn(1).setPreferredWidth(60);
+        VentanaDeUsuario.tabla.getColumnModel().getColumn(2).setPreferredWidth(60);
+        VentanaDeUsuario.tabla.getColumnModel().getColumn(3).setPreferredWidth(160);
+        VentanaDeUsuario.tabla.getColumnModel().getColumn(4).setPreferredWidth(50);
+        VentanaDeUsuario.tabla.getColumnModel().getColumn(5).setPreferredWidth(60);
+        VentanaDeUsuario.tabla.getColumnModel().getColumn(6).setPreferredWidth(60);
+    }
+    
+    
+    
+    
      
 }
--- src/uy/gruposoft/persistencia/PersistenciaUsuario.java
+++ src/uy/gruposoft/persistencia/PersistenciaUsuario.java
@@ -5,30 +5,87 @@
  */
 package uy.gruposoft.persistencia;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import uy.gruposoft.logica.Usuario;
-import uy.gruposoft.logica.LogicaUsuarios;
+
 /**
  *
  * @author Administrador
  */
 public class PersistenciaUsuario {
-    
-    public LogicaUsuarios listaUsuarios() {
 
-        //paso 1 : crear la conexion a la base
-        //paso 2 : crear el prepare statement
-        //paso 3 : ejecutar la consulta del preparestatement
-        //paso 4 : cargar los resultados en los objetos de la capa logica si es un select la consulta
-        //paso 5 : cerrar la conexion a la base
-        LogicaUsuarios usuarios = new LogicaUsuarios();
+    public static DefaultTableModel mostrarUsuarios(String[] columnas) {
+        DefaultTableModel modelo = new DefaultTableModel(null,columnas);
+        String[] registros = new String[7];
+        
 
-        return usuarios;
+       
 
+        final String sql = "SELECT * FROM usuarios";
+
+        Connection cn = null;
+
+        PreparedStatement pst = null;
+        //ArrayList<Usuario> usuarios= new ArrayList();
+
+        ResultSet rs = null;
+
+        try {
+            cn = Conexion.conectar();
+
+            pst = cn.prepareStatement(sql);
+
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                //Usuario usuario=new Usuario();
+                
+                
+                registros[0] = rs.getString("username");
+                //usuario.setNombre(rs.getString("username"));
+
+                registros[1] = rs.getString("nombre");
+
+                registros[2] = rs.getString("apellido");
+
+                registros[3] = rs.getString("email");
+                registros[4] = rs.getString("contraseña");
+                registros[5] = rs.getString("fecha_alta");
+                registros[6] = rs.getString("fecha_baja");
+
+              modelo.addRow(registros);
+              //usuarios.add(usuario);
+
+            }
+
+        } catch (SQLException e) {
+
+            JOptionPane.showMessageDialog(null, "Error al conectar");
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+
+                if (pst != null) {
+                    pst.close();
+                }
+
+                if (cn != null) {
+                    cn.close();
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
+            }
+        }
+        return modelo;
     }
 
     public static Boolean existeUsuario(Usuario usuario) {
@@ -49,7 +106,7 @@ public class PersistenciaUsuario {
         ResultSet rs = null;
         try {
             Connection conexion = con.conectar();
-            String sqlStm = "select * from usuarios where username='" + nombre + "' and contraseña='" + clave + "';";
+            final String sqlStm = "select * from usuarios where username='" + nombre + "' and contraseña='" + clave + "';";
             ps = conexion.prepareStatement(sqlStm);
             rs = ps.executeQuery();
             if (rs.next()) {
@@ -87,10 +144,5 @@ public class PersistenciaUsuario {
         //paso 3 : ejecutar la consulta del preparestatement
         //paso 5 : cerrar la conexion a la base
     }
-    
-    
-    
-    
-    
-    
+
 }
