c220a649f99edacfdfa0d8560a6df2fb2b9d0203
Guillermo Delgado <guillermod2804@gmail.com>
2022-07-26 23:58:57 -0300

    modifico LOG en ventana Login
    Log en consola.

--- src/uy/gruposoft/presentacion/Login.java
+++ src/uy/gruposoft/presentacion/Login.java
@@ -9,6 +9,8 @@ import java.awt.Desktop;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import uy.gruposoft.logica.FachadaLogica;
@@ -119,8 +121,10 @@ public class Login extends javax.swing.JFrame {
 //       //invoco al existeUsuario con el usuario que cargue en memoria para saber si existe en la base 
         Boolean existe = FachadaLogica.existeUsuario(usuario);
         if (existe) {
-
-            System.out.println("LOG - Login: " + nombre + " se conecto");
+	    
+	    LocalDateTime dateTime = LocalDateTime.now();
+	    String timeStamp = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss").format(dateTime);
+            System.out.println(timeStamp + " ||INFO|| " + "--" + " ||Login|| " + "Message: " + nombre + " se conecto");
 
             JLabel mensajeLbl = new JLabel();
 
@@ -130,7 +134,9 @@ public class Login extends javax.swing.JFrame {
         } else {
 
             //System.out.println("Usuario no encontrado");
-            System.out.println("LOG - Login_ERROR: " + nombre + " no encontrado");
+	    LocalDateTime dateTime = LocalDateTime.now();
+	    String timeStamp2 = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss").format(dateTime);
+            System.out.println(timeStamp2 + " ||ERROR|| " + "--" + " ||Login|| " + "Message: " + nombre + " no encontrado");
             JOptionPane.showMessageDialog(null, "Usuario no encontrado");
             nombreUsuario.setText("");
             clave.setText("");
