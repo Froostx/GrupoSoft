f49825d76cd62e6dfe1a5f15d9a10a617bba7db0
andressantu <89048886+andressantu@users.noreply.github.com>
2022-08-05 17:18:31 -0300

    deudores-solo funcion de añador nuevos

---
+++ src/uy/gruposoft/persistencia/PersistenciaDeudor.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.gruposoft.persistencia;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import uy.gruposoft.excepciones.DeudorException;
+import uy.gruposoft.logica.Deudor;
+import uy.gruposoft.logica.Deudores;
+
+/**
+ *
+ * @author andressantucci
+ */
+public class PersistenciaDeudor {
+    
+    
+    
+    private static final String all = "SELECT * FROM deudores";
+    private static final String verificar = "SELECT ci_afiliado FROM grupo_soft.deudores WHERE ci_afiliado = ?";
+    private static final String insert = "INSERT INTO grupo_soft.deudores (ci_afiliado, cant_meses) VALUES (?, ?)";
+
+    
+    
+    static Connection cn = null;
+    static Conexion conexion = new Conexion();
+
+    static PreparedStatement pst = null;
+    
+    
+    
+    
+    public static Deudores mostrarDeudores() throws DeudorException, SQLException {
+
+        Deudores deudores = new Deudores();
+
+        ResultSet rs = null;
+
+        try {
+            cn = Conexion.conectar();
+
+            pst = cn.prepareStatement(all);
+
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                Deudor deudor = new Deudor();
+                deudor.setCiAfiliado(rs.getInt("ci_afiliado"));
+                deudor.setCantMeses(rs.getInt("cant_meses"));
+               
+                deudores.agregarDeudor(deudor);
+
+            }
+
+        } catch (SQLException e) {
+
+            throw new DeudorException("No se pudo cargar los deudores");
+
+        } finally {
+           
+                if (rs != null) {
+                    rs.close();
+                }
+
+                if (pst != null) {
+                    pst.close();
+                }
+
+                if (cn != null) {
+                    cn.close();
+                }
+  
+        }
+        return deudores;
+    }
+    
+    
+    public static boolean verificarDeudor(Deudor deudor) throws DeudorException
+    {
+        boolean existe = false;
+        try{
+            cn = conexion.conectar();
+            pst = cn.prepareStatement(verificar);
+            pst.setInt(1, deudor.getCiAfiliado());
+            
+            ResultSet rs = pst.executeQuery();
+            if (rs.next())
+                existe = true;
+        
+        } catch(SQLException  e){
+            throw new DeudorException("No encontre el deudpr");
+        }
+    
+        return existe;
+    }
+    
+    public static void altaDeudor(Deudor deudor) throws DeudorException{
+        try{
+            
+        cn = conexion.conectar();
+        pst = cn.prepareStatement(insert);
+        pst.setInt(1,deudor.getCiAfiliado());
+        pst.setInt(2, deudor.getCantMeses());
+        
+        pst.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DeudorException("No pude insertar el deudor");
+        }
+    }
+    
+}
--- src/uy/gruposoft/presentacion/MenuPrincipal.java
+++ src/uy/gruposoft/presentacion/MenuPrincipal.java
@@ -9,12 +9,14 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JInternalFrame;
 import javax.swing.JOptionPane;
 import uy.gruposoft.excepciones.AfiliadoException;
+import uy.gruposoft.excepciones.DeudorException;
 import uy.gruposoft.excepciones.LocalException;
 import uy.gruposoft.excepciones.UsuarioException;
 
@@ -58,6 +60,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
         locales = new javax.swing.JMenuItem();
         Usuario = new javax.swing.JMenuItem();
         Negocio = new javax.swing.JMenuItem();
+        deudores = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem6 = new javax.swing.JMenuItem();
         jMenuItem7 = new javax.swing.JMenuItem();
@@ -89,7 +92,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
         jMenu1.setText("Formularios");
 
-        afiliados.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_1, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        afiliados.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_1, java.awt.event.InputEvent.CTRL_MASK));
         afiliados.setText("Afiliados");
         afiliados.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -98,7 +101,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
         });
         jMenu1.add(afiliados);
 
-        locales.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_2, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        locales.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_2, java.awt.event.InputEvent.CTRL_MASK));
         locales.setText("Locales");
         locales.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -107,7 +110,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
         });
         jMenu1.add(locales);
 
-        Usuario.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_3, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        Usuario.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_3, java.awt.event.InputEvent.CTRL_MASK));
         Usuario.setText("Usuarios");
         Usuario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -116,9 +119,23 @@ public class MenuPrincipal extends javax.swing.JFrame {
         });
         jMenu1.add(Usuario);
 
-        Negocio.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_4, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        Negocio.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_4, java.awt.event.InputEvent.CTRL_MASK));
         Negocio.setText("Negocio");
+        Negocio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NegocioActionPerformed(evt);
+            }
+        });
         jMenu1.add(Negocio);
+
+        deudores.setActionCommand("Deudores");
+        deudores.setLabel("Deudores");
+        deudores.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deudoresActionPerformed(evt);
+            }
+        });
+        jMenu1.add(deudores);
 
         jMenuBar1.add(jMenu1);
 
@@ -261,6 +278,35 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         dispose();
     }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void deudoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deudoresActionPerformed
+        // TODO add your handling code here:
+        VentanaDeudores verDeudores = null;
+        try {
+            
+            verDeudores = new VentanaDeudores();
+            
+        } catch (DeudorException ex) {
+            Logger.getLogger(MenuPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MenuPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (JInternalFrames_AbiertosDeudor(verDeudores) == false) { 
+            Ventanas.add(verDeudores);
+            
+                      
+            Dimension desktopSize = Ventanas.getSize();
+            Dimension FrameSize = verDeudores.getSize();
+            verDeudores.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
+            verDeudores.show();
+    }//GEN-LAST:event_deudoresActionPerformed
+
+    }
+    
+    private void NegocioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NegocioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_NegocioActionPerformed
     
     
     public boolean JInternalFrames_AbiertosLocal(VentanaLocales jif) { 
@@ -288,6 +334,22 @@ public class MenuPrincipal extends javax.swing.JFrame {
     }
     return false;
     }
+    
+    
+    
+    public boolean JInternalFrames_AbiertosDeudor(VentanaDeudores jif) { 
+    JInternalFrame[] jif_Activos = MenuPrincipal.Ventanas.getAllFrames();
+
+    for (int i = 0; i < jif_Activos.length; i++) {
+
+        if (jif.getClass().isInstance(jif_Activos[i])) {
+            JOptionPane.showMessageDialog(null, "La ventana que esta intentando abrir ya esta abierta.", "Información", JOptionPane.INFORMATION_MESSAGE);
+            return true;
+        }
+    }
+    return false;
+    }
+    
     
 
     /**
@@ -331,6 +393,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem Usuario;
     private static javax.swing.JDesktopPane Ventanas;
     private javax.swing.JMenuItem afiliados;
+    private javax.swing.JMenuItem deudores;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
